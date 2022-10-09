a37e94690056bd5322019cf062c44c58f4e80fa3
Johnapolitan <71734690+Johnapolitan@users.noreply.github.com>
2022-10-09 03:16:55 -0400

    cleanup code + change linter rules

--- discord.js
+++ discord.js
@@ -1,7 +1,3 @@
-/* eslint-disable new-cap */
-/* eslint-disable guard-for-in */
-/* eslint-disable require-jsdoc */
-/* eslint-disable linebreak-style */
 /**
  * Copyright 2020 Google LLC
  *
@@ -18,26 +14,24 @@
  * limitations under the License.
  */
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Set your emoji "awards" here
-const {Client, GatewayIntentBits} = require('discord.js');
-const {guildId, token} = require('./config.json');
-const {EmbedBuilder} = require('discord.js');
-const perspective = require('./perspective.js');
+const { Client, GatewayIntentBits } = require("discord.js");
+const { guildId, token } = require("./config.json");
+const { EmbedBuilder } = require("discord.js");
+const perspective = require("./perspective.js");
 
 const emojiMap = {
-  'FLIRTATION': '🥰',
-  'TOXICITY': '🧨',
-  'INSULT': '☠',
-  'INCOHERENT': '🤷',
-  'SPAM': '🤖',
+	"FLIRTATION": "🥰",
+	"TOXICITY": "🧨",
+	"INSULT": "☠",
+	"INCOHERENT": "🤷",
+	"SPAM": "🤖",
 };
 
-const BotIcon = 'https://images-ext-1.discordapp.net/external/vzl3NGWAEK1Te1Gad7T5iMDtCSNZctkSGApvhD6JoxM/https/cdn.discordapp.com/embed/avatars/2.png';
+const BotIcon = "https://images-ext-1.discordapp.net/external/vzl3NGWAEK1Te1Gad7T5iMDtCSNZctkSGApvhD6JoxM/https/cdn.discordapp.com/embed/avatars/2.png";
 
-// Store some state about user karma.
-// TODO: Migrate to a DB, like Firebase
 const users = {};
 
 /**
@@ -46,12 +40,13 @@ const users = {};
  * @param {guild} guild - guild to kick user from
  */
 async function kickBaddie(user, guild) {
-  if (!user) return;
-  try {
-    await user.kick('Was a jerk');
-  } catch (err) {
-    console.log(`Could not kick user ${user.author}: ${err}`);
-  }
+	if (!user) return;
+	try {
+		await user.kick("Was a jerk");
+	}
+	catch (err) {
+		console.log(`Could not kick user ${user.author}: ${err}`);
+	}
 }
 
 /**
@@ -62,26 +57,27 @@ async function kickBaddie(user, guild) {
  * kick the users
  */
 async function evaluateMessage(message) {
-  let scores;
-  try {
-    scores = await perspective.analyzeText(message.content);
-  } catch (err) {
-    console.log(err);
-    return false;
-  }
+	let scores;
+	try {
+		scores = await perspective.analyzeText(message.content);
+	}
+	catch (err) {
+		console.log(err);
+		return false;
+	}
 
-  const userid = message.author.id;
+	const userid = message.author.id;
 
-  for (const attribute in emojiMap) {
-    if (scores[attribute]) {
-      message.react(emojiMap[attribute]);
-      users[userid][attribute] =
-                users[userid][attribute] ?
-                users[userid][attribute] + 1 : 1;
-    }
-  }
-  // Return whether or not we should kick the user
-  return (users[userid]['TOXICITY'] > process.env.KICK_THRESHOLD);
+	for (const attribute in emojiMap) {
+		if (scores[attribute]) {
+			message.react(emojiMap[attribute]);
+			users[userid][attribute] =
+			users[userid][attribute] ?
+				users[userid][attribute] + 1 : 1;
+		}
+	}
+	// Return whether or not we should kick the user
+	return (users[userid]["TOXICITY"] > process.env.KICK_THRESHOLD);
 }
 
 /**
@@ -89,138 +85,132 @@ async function evaluateMessage(message) {
  * @return {string} karma - printable karma scores
  */
 function getKarma() {
-  const scores = [];
-  let ToReturn = '';
-  for (const user in users) {
-    if (!Object.keys(users[user]).length) continue;
+	const scores = [];
+	let ToReturn = "";
+	for (const user in users) {
+		if (!Object.keys(users[user]).length) continue;
 
-    let score = `<@${user}> - `;
-    for (const attr in users[user]) {
-      score += `${emojiMap[attr]} : ${users[user][attr]}\t`;
-    }
-    scores.push(score);
-  }
-  console.log(scores);
-  if (!scores.length) {
-    return '';
-  }
-  if (scores.length === 0) {
-    ToReturn = '';
-  } else {
-    ToReturn = new EmbedBuilder()
-    .setColor('#ffd200')
-    .addFields(
-        { name: 'Karma Leaderboard', value: `${ scores.join('\n') }` }, 
-    )
-    .setTimestamp();
-  }
-  console.log(scores)
-  // return scores.join('\n');
-  return ToReturn
+		let score = `<@${user}> - `;
+		for (const attr in users[user]) {
+			score += `${emojiMap[attr]} : ${users[user][attr]}\t`;
+		}
+		scores.push(score);
+	}
+	if (!scores.length) {
+		return "";
+	}
+	if (scores.length === 0) {
+		ToReturn = "";
+	}
+	else {
+		ToReturn = new EmbedBuilder()
+			.setColor("#ffd200")
+			.addFields(
+				{ name: "Karma Leaderboard", value: `${ scores.join("\n") }` },
+			)
+			.setTimestamp();
+	}
+	return ToReturn;
 }
 
 // Create an instance of a Discord client
-const client = new Client({intents: [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.MessageContent,
-  GatewayIntentBits.GuildMembers],
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent,
+	GatewayIntentBits.GuildMembers],
 });
 
-client.on('ready', () => {
-  console.log('Ready!');
+client.on("ready", () => {
+	console.log("Ready!");
 });
 
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
 
-  const {commandName} = interaction;
+	const { commandName } = interaction;
 
-  if (commandName === 'ping') {
-    await interaction.reply(`Ping: ${client.ws.ping}ms.`);
+	if (commandName === "ping") {
+		await interaction.reply(`Ping: ${client.ws.ping}ms.`);
+	}
+	else if (commandName === "serverinfo") {
+		const exampleEmbed = new EmbedBuilder()
+			.setColor("#ffd200")
+			.setTitle("Server Info")
+			.setThumbnail(BotIcon)
+			.addFields(
+				{ name: "Creation Date", value: `${interaction.guild.createdAt.toDateString()}`, inline: true },
+				{ name: "Owner", value: "johnb_tch#5577", inline: true },
+				{ name: "Member Count", value: `${getNumberOfMembers()}`, inline: true },
+			)
+			.setTimestamp()
+			.setFooter({ text: `Requested by: ${interaction.user.tag}`, iconURL: "https://cdn.discordapp.com/avatars/" + interaction.user.id + "/" + interaction.user.avatar + ".png" });
 
-  } else if (commandName === 'serverinfo') {
-    const exampleEmbed = new EmbedBuilder()
-        .setColor('#ffd200')
-        .setTitle('Server Info')
-        .setThumbnail(BotIcon)
-        .addFields(
-            {name: 'Creation Date', value: `${interaction.guild.createdAt.toDateString()}`, inline: true},
-            {name: 'Owner', value: 'johnb_tch#5577', inline: true},
-            {name: 'Member Count', value: `${GetNumberOfMembers()}`, inline: true},
-        )
-        .setTimestamp()
-        .setFooter({text: `Requested by: ${interaction.user.tag}`, iconURL: 'https://cdn.discordapp.com/avatars/' + interaction.user.id + '/' + interaction.user.avatar + '.png'});
+		await interaction.reply({ embeds: [exampleEmbed] });
+	}
+	else if (commandName === "user") {
+		const targetUser = interaction.options.getUser("user");
+		const userPfp = "https://cdn.discordapp.com/avatars/" + targetUser.id + "/" + targetUser.avatar + ".png";
+		const member = interaction.guild.members.cache.get(targetUser.id);
+		const exampleEmbed = new EmbedBuilder()
+			.setColor(targetUser.accentColor || "#ffd200")
+			.setTitle(targetUser.tag)
+			.setThumbnail(userPfp)
+			.addFields(
+				{ name: "Registered", value: `${targetUser.createdAt.toDateString()}`, inline: false },
+				{ name: "Joined", value: `${member.joinedAt.toDateString()}`, inline: false },
+			)
+			.setTimestamp()
+			.setFooter({ text: `Requested by: ${interaction.user.tag}`, iconURL: "https://cdn.discordapp.com/avatars/" + interaction.user.id + "/" + interaction.user.avatar + ".png" });
 
-    await interaction.reply({embeds: [exampleEmbed]});
-
-  } else if (commandName === 'user') {
-    const targetUser = interaction.options.getUser('user');
-    const userPfp = 'https://cdn.discordapp.com/avatars/' + targetUser.id + '/' + targetUser.avatar + '.png';
-    const member = interaction.guild.members.cache.get(targetUser.id);
-    const exampleEmbed = new EmbedBuilder()
-        .setColor(targetUser.accentColor || '#ffd200')
-        .setTitle(targetUser.tag)
-        .setThumbnail(userPfp)
-        .addFields(
-            {name: 'Registered', value: `${targetUser.createdAt.toDateString()}`, inline: false},
-            {name: 'Joined', value: `${member.joinedAt.toDateString()}`, inline: false},
-        )
-        .setTimestamp()
-        .setFooter({text: `Requested by: ${interaction.user.tag}`, iconURL: 'https://cdn.discordapp.com/avatars/' + interaction.user.id + '/' + interaction.user.avatar + '.png'});
-
-    await interaction.reply({embeds: [exampleEmbed]});
-
-  } else if (commandName === 'userid') {
-    const targetUser = interaction.options.getUser('user');
-    await interaction.reply({content: `${targetUser.id}`, ephemeral: true});
-
-  } else if (commandName === 'karma') {
-    const karma = getKarma();
-    if (typeof(karma) === "string") {
-      await interaction.reply("No karma yet!");
-    } else {
-      await interaction.reply({embeds: [karma]});
-    }
-  }
+		await interaction.reply({ embeds: [exampleEmbed] });
+	}
+	else if (commandName === "userid") {
+		const targetUser = interaction.options.getUser("user");
+		await interaction.reply({ content: `${targetUser.id}`, ephemeral: true });
+	}
+	else if (commandName === "karma") {
+		const karma = getKarma();
+		if (typeof (karma) === "string") {
+			await interaction.reply("No karma yet!");
+		}
+		else {
+			await interaction.reply({ embeds: [karma] });
+		}
+	}
 });
 
-client.on('messageCreate', async (message) => {
-  // Ignore messages that aren't from a guild
-  // or are from a bot
-  if (!message.guild || message.author.bot) return;
-  if (message.content === '') return;
+client.on("messageCreate", async (message) => {
+	if (!message.guild || message.author.bot) return;
+	if (message.content === "") return;
 
-  // If we've never seen a user before, add them to memory
-  const userid = message.author.id;
-  if (!users[userid]) {
-    users[userid] = [];
-  }
+	const userid = message.author.id;
+	if (!users[userid]) {
+		users[userid] = [];
+	}
 
-  // Evaluate attributes of user's message
-  let shouldKick = false;
-  try {
-    shouldKick = await evaluateMessage(message);
-  } catch (err) {
-    console.log(err);
-  }
-  if (shouldKick) {
-    const member = message.member;
+	let shouldKick = false;
+	try {
+		shouldKick = await evaluateMessage(message);
+	}
+	catch (err) {
+		console.log(err);
+	}
+	if (shouldKick) {
+		const member = message.member;
 
-    kickBaddie(member, message.guild);
-    delete users[message.author.id];
+		kickBaddie(member, message.guild);
+		delete users[message.author.id];
 
-    message.channel.send(`Kicked user ${message.author.username} from channel`);
-    return;
-  }
+		message.channel.send(`Kicked user ${message.author.username} from channel`);
+		return;
+	}
 });
 
 
-// // Log our bot in using the token from https://discordapp.com/developers/applications/me
-// client.login(process.env.DISCORD_TOKEN);
-function GetNumberOfMembers() {
-  const guild = client.guilds.cache.get(guildId);
-  return guild.memberCount;
+function getNumberOfMembers() {
+	const guild = client.guilds.cache.get(guildId);
+	return guild.memberCount;
 }
 
 client.login(token);
